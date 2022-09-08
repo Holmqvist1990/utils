19bc4dcbd5fd9fc6176e38bf85f7200db5ddfca6
Holmqvist1990 <fredrik.d.holmqvist@gmail.com>
2022-09-08 14:02:16 +0200

    feature: more clamps, lerps, sqr

--- math.go
+++ math.go
@@ -31,6 +31,10 @@ func Magnitude[T Number](x, y T) T {
 	return T(math.Sqrt(_x*_x + _y*_y))
 }
 
+func Sqr[T Number](t T) T {
+	return t * t
+}
+
 func Normalize[T Float](x, y T) (T, T) {
 	if x == 0 || y == 0 {
 		return x, y
@@ -48,6 +52,14 @@ func Clamp[T Number](n, low, high T) T {
 	return Max(low, Min(n, high))
 }
 
+func ClampFloor[T Number](n, low T) T {
+	return Max(n, low)
+}
+
+func ClampCeiling[T Number](n, high T) T {
+	return Min(n, high)
+}
+
 // Minimum absolute value or zero.
 func MinOrZero[T Number](v, min T) T {
 	if Abs(v) < min {
@@ -59,4 +71,24 @@ func MinOrZero[T Number](v, min T) T {
 
 func Lerp[T Number](start, end, t T) T {
 	return (start + (end-start)*t)
+}
+
+func LerpEaseIn[T Number](start, end, t T) T {
+	return Lerp(start, end, easeIn(t))
+}
+
+func LerpEaseOut[T Number](start, end, t T) T {
+	return Lerp(start, end, easeOut(t))
+}
+
+func flip[T Number](t T) T {
+	return 1 - t
+}
+
+func easeIn[T Number](t T) T {
+	return Sqr(t)
+}
+
+func easeOut[T Number](t T) T {
+	return flip(Sqr(flip(t)))
 }
